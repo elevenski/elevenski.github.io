4b2883a7a9c072c02ce425ad139219965f647f3e
eleven <50018633+elevendev@users.noreply.github.com>
2020-10-18 21:51:42 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static("public"));
+  app.use(
+    "/css",
+    express.static(path.resolve(__dirname + `/css`))
+  );
+
+  app.use(
+    "/js",
+    express.static(path.resolve(__dirname + `/js`))
+  );
+  
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "index.html");
+});
+app.get("/*", (request, response) => {
+  response.sendFile(__dirname + "404.html");
+});
