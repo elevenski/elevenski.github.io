ca7975b0ba4a7796ca270b9f4f5044e3f2c79730
eleven <50018633+elevendev@users.noreply.github.com>
2020-10-20 16:36:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -18,9 +18,6 @@ app.use(express.static("public"));
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "index.html");
 });
-app.get("/weg", (request, response) => {
-  response.sendFile(__dirname + "weg.html");
-});
 app.get("/contributors", (request, response) => {
   response.sendFile(__dirname + "contributors.html");
 });
