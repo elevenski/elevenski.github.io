e9ae36ffb9f6fb5d4b2f81fd826e7d6ddb710393
Eleven <elevenvac@gmail.com>
2021-04-23 20:37:04 +0300

    Update eggo.js

--- eggo.js
+++ eggo.js
@@ -2,6 +2,9 @@ new Vue({
     el: "#app",
     data: {
         version: "v1.0.3967-stable",
+        prefix: "11",
+        example_command: "help",
+        total_commands: "14",
         author: {
             developer: "Eleven",
             developer_2: "Eleven 2"
