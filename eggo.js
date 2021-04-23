4cabdca5a979de51238b66a92720c7d43b2e8b3e
Eleven <elevenvac@gmail.com>
2021-04-23 20:31:24 +0300

    Create eggo.js

---
+++ eggo.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: "#app",
+    data: {
+        version: "v1.0.3967-stable",
+        author: {
+            developer: "Eleven",
+            developer_2: "Eleven 2"
+        },
+    }
+});
