69998c8533d736a4412caec7379dd397a3a98dba
Eleven <elevenvac@pm.me>
2022-03-18 09:22:04 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static("public"));
+  /*app.use(
+    "/css",
+    express.static(path.resolve(__dirname + `/css`))
+  );
+
+  app.use(
+    "/js",
+    express.static(path.resolve(__dirname + `/js`))
+  );*/
+
+app.get("/*", (req, res) => {
+  res.render("/e/404.html")
+});
