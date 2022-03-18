303cc0817817c363f810338f7bc4cc3e75a58f26
Eleven <elevenvac@pm.me>
2022-03-18 10:31:41 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
-const express = require("express");
+const config = require("platformsh-config").config();
+const express = require('express')
+const app = express()
+
+app.get('/*', (req, res) => {
+    res.status(404).send('Sorry, that page cannot be found.')
+})
+app.listen(config.port, () => {
+    console.log(`[INFO]: ExpressJS listening on ${config.port}.`)
+})
+
+/*const express = require("express");
 const app = express();
 const path = require("path");
 
 app.use(express.static("public"));
-  /*app.use(
+  app.use(
     "/css",
     express.static(path.resolve(__dirname + `/css`))
   );
@@ -11,8 +22,9 @@ app.use(express.static("public"));
   app.use(
     "/js",
     express.static(path.resolve(__dirname + `/js`))
-  );*/
+  );
 
 app.get("/*", (req, res) => {
   res.send("404 Not Found")
 });
+*/
