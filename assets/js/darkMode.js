1a31e5daab859ba2a3bdc74d6b7d873a5fac0904
Eleven <elevenvac@pm.me>
2022-03-19 13:16:15 +0300

    lshift + t: sytem dark/light combination & improvements

--- assets/js/darkMode.js
+++ assets/js/darkMode.js
@@ -15,7 +15,7 @@ toggleBtn.addEventListener('click', () => {
     }
 });
 
-$(document).ready(function(){
+$(document).ready(function () {
     console.log("%c Eleven", "background: transparent; color: #b5b5e7; font-size: 30px; font-weight: 500")
     console.log("%c Temalar yükleniyor...", "background: transparent; color: red; font-size: 18px; font-weight: 400")
     let theme = localStorage.getItem("data-theme");
@@ -27,5 +27,31 @@ $(document).ready(function(){
         document.documentElement.classList.remove('dark-theme');
         console.log("light moda geçildi")
         document.getElementById("switch-mode-btn").innerHTML = "<i class='fa-solid fa-moon mr-2'></i>Switch Dark Mode"
+    }
+});
+
+document.addEventListener("keydown", function (zEvent) {
+    let theme = localStorage.getItem("data-theme");
+    if (zEvent.shiftKey && zEvent.key === "T") {
+        const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
+        if (darkThemeMq.matches) {
+            if (theme === "dark") {
+                console.log("site zaten dark")
+            } else {
+                localStorage.setItem("data-theme", "dark");
+                document.documentElement.classList.toggle('dark-theme');
+                console.log("system dark moda geçildi")
+                document.getElementById("switch-mode-btn").innerHTML = "<i class='fa-solid fa-sun mr-2'></i>Switch Light Mode"
+            }
+        } else if (!darkThemeMq.matches) {
+            if (theme === "light") {
+                console.log("site zaten light")
+            } else {
+                localStorage.setItem("data-theme", "light");
+                document.documentElement.classList.remove('dark-theme');
+                console.log("system light moda geçildi")
+                document.getElementById("switch-mode-btn").innerHTML = "<i class='fa-solid fa-moon mr-2'></i>Switch Dark Mode"
+            }
+        }
     }
 });
