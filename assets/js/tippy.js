1efa9dfca6340205ff690a94eb0d8326f53e00db
Eleven <elevenvac@pm.me>
2022-03-19 10:42:07 +0300

    improvements

--- assets/js/tippy.js
+++ assets/js/tippy.js
@@ -1,19 +1,9 @@
-/*tippy('#goTop', {
-    content: '<p id="goTopContent" class="ml-1 mr-1">Go page top</p>',
-    animation: 'scale',
-    placement: 'left',
-    theme: 'ws',
-    inertia: true,
-    //interactive: true,
-    allowHTML: true
-});*/
-
 tippy('#statusContent', {
     content: '<p class="ml-1 mr-1">Discord status</p>',
     animation: 'scale',
     placement: 'bottom',
     theme: 'ws',
     inertia: true,
-    //interactive: true,
+    interactive: false,
     allowHTML: true
 });
