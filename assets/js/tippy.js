ec26bbcacbb6d32d93ce98d769d5f401d518c733
Eleven <elevenvac@pm.me>
2022-04-02 22:34:47 +0300

    !! BETA BETA BETA BETA !! daily activity (in school/bed etc.) !! BETA BETA BETA BETA BETA BETA BETA BETA BETA BETA BETA BETA BETA BETA BETA BETA BETA BETA BETA BETA BETA BETA BETA BETA BETA BETA BETA BETA !!

--- assets/js/tippy.js
+++ assets/js/tippy.js
@@ -17,3 +17,13 @@ tippy('#command-palette-btn', {
     interactive: false,
     allowHTML: true
 });
+
+tippy('#date', {
+    content: '<p class="ml-1 mr-1">Turkey/Istanbul Time</p>',
+    animation: 'scale',
+    placement: 'bottom',
+    theme: 'ws',
+    inertia: true,
+    interactive: false,
+    allowHTML: true
+});
