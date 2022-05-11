ef9b748b9ec4ff7e46969462c76ef11535ab931a
Eleven <elevenvac@pm.me>
2022-05-11 19:27:46 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const app = express();
 
+var minifyHTML = require('express-minify-html');
+router.use(minifyHTML({
+    override:      true,
+    exception_url: false,
+    htmlMinifier: {
+        removeComments:            true,
+        collapseWhitespace:        true,
+        collapseBooleanAttributes: true,
+        removeAttributeQuotes:     true,
+        removeEmptyAttributes:     true,
+        minifyJS:                  true
+    }
+}));
+
 app.use(express.static("public"));
 
 app.get("/", (request, response) => {
