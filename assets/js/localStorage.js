d7ecb65ee9186f6bfe19b0512e7e1c08989bff07
Eleven <elevenvac@pm.me>
2022-09-09 23:25:54 +0300

    Delete assets directory

--- assets/js/localStorage.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("keydown", function (zEvent) {
-    if (zEvent.shiftKey && zEvent.key === "C") {
-        localStorage.clear();
-        location.reload();
-        console.log("tüm yerel depolamalar silindi")
-    }
-});
