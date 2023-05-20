8cc67ac7144361832fde6db8f0d9a04259077495
Eleven <elevenvac@pm.me>
2023-05-20 11:31:21 +0300

    Update lanyard.js

--- assets/js/lanyard.js
+++ assets/js/lanyard.js
@@ -15,7 +15,7 @@ lanyard.onopen = function () {
     JSON.stringify({
       op: 2,
       d: {
-        subscribe_to_id: "354343248698802187",
+        subscribe_to_id: "692681908873723965",
       },
     })
   );
